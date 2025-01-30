13da2f0069e02c030357f7c0a3cd3dda3d80228c
Ma Thandar <mamatda@gmail.com>
2025-01-30 15:06:28 +0630

    Update prompt_test.py

--- prompt_test.py
+++ prompt_test.py
@@ -1,6 +1,7 @@
 #pip install langchain-groq
 #pip install langchain
 import streamlit as st
+import chromadb
 from langchain_groq import ChatGroq
 
 llm = ChatGroq(    
@@ -12,3 +13,7 @@ response = llm.invoke("the man who walked on the moon first")
 st.write(response.content)
 #groq_api_key='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',
 #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
+
+
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="my_collection")
