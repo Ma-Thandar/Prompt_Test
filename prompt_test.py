b089b23385fcb5ee051f3ac383ee41fcb9512292
Ma Thandar <mamatda@gmail.com>
2025-01-30 15:32:26 +0630

    Update prompt_test.py

--- prompt_test.py
+++ prompt_test.py
@@ -9,22 +9,15 @@ import sqlite3
 
 st.write("Current Working Directory:", os.getcwd())
 
-# Install ChromaDB dynamically (for debugging)
-try:
-    import chromadb
-except ImportError:
-    subprocess.run(["pip", "install", "chromadb"], check=True)
-    import chromadb
 
-print("ChromaDB imported successfully!")
 
-#llm = ChatGroq(    
-#    temperature=0,
-#    groq_api_key='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',    
-#    model_name='llama-3.1-8b-instant'  
-#)
-#response = llm.invoke("the man who walked on the moon first")
-#st.write(response.content)
+llm = ChatGroq(    
+    temperature=0,
+    groq_api_key='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',    
+    model_name='llama-3.1-8b-instant'  
+)
+response = llm.invoke("the man who walked on the moon first")
+st.write(response.content)
 #groq_api_key='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',
 #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
 
