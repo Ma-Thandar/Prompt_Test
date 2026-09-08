56c73ca2a14ec33a882d8f9039c9344e9dbecc88
Ma Thandar <mamatda@gmail.com>
2026-09-08 10:55:51 +0630

    Update prompt_test.py

--- prompt_test.py
+++ prompt_test.py
@@ -1,26 +1,15 @@
 #pip install langchain-groq
 #pip install langchain
-import langchain
 import streamlit as st
 from langchain_groq import ChatGroq
-import os
-import subprocess
-import sqlite3
-
-st.write("Current Working Directory:", os.getcwd())
-
-
 
 llm = ChatGroq(    
     temperature=0,
     groq_api_key='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',    
-    model_name='llama-3.1-8b-instant'  
+    model_name='openai/gpt-oss-120b'  
 )
 response = llm.invoke("the man who walked on the moon first")
-st.write(response.content)
+print(response.content)
+st.write(response.content)    
 #groq_api_key='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',
 #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
-
-
-#chroma_client = chromadb.Client()
-#collection = chroma_client.create_collection(name="my_collection")
